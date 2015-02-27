36007839fe68de6634af28ae49cd1f5a7d3f5b7f
jaioneirizar <jaioner14@gmail.com>
2015-02-27 11:24:04 +0100

    Ejercicio Final Java Script

--- JavaScript/ejfinal/assets/js/validator.js
+++ JavaScript/ejfinal/assets/js/validator.js
@@ -1,105 +1,116 @@
 HTMLFormElement.prototype.validate = function() {
 
-    //var form = this;
-
+    var required = this.querySelectorAll(".required");
+    var email = this.querySelectorAll(".email");
+    var errores = [];
 
     var validador = {
 
         required: function(val) {
-            return !(val === undefined || val === null || val.length > 0 || /^\s+$/.test(val));
+            return (val !== undefined && val !== null && val.length > 0 && !/^\s+$/.test(val));
         },
         email: function(val) {
 
-            return /^\w+(\.\w+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/.test(val);
+
+            return /^\w([\w.\-]*\w)?@[a-zA-Z0-9]([\w.\-]*\w)?\.[a-zA-Z]{2,3}$/.test(val);
+
 
         },
 
         password: function(val) {
 
-            may = /[A-Z]/;
-            minuscula = /[a-z]/;
-            digito = /[0-9]/;
-
-           // return this.required(value) && (val.length >= 6) && may.test(val) && minuscula.test(val) && digito.test(val);
+            return this.required(val) &&
+                val.length >= 6 &&
+                /[a-z]/.test(val) &&
+                /[A-Z]/.test(val) &&
+                /[0-9]/.test(val);
         }
 
 
     };
-
-
-
-    var required = this.querySelectorAll(".required");
-    var email = this.querySelectorAll(".email");
-
 
     var validate = function(e) {
 
-        var errores = [];
+        for (var i = required.length - 1; i >= 0; i--) {
+            //Si es de tipo password le llamamos a la utilidad password del objeto validator
+            validateField.call(required[i]);
+        }
 
-        
+        for (var n = email.length - 1; n >= 0; n--) {
 
 
-     for (var i = email.length - 1; i >= 0; i--) {
-
-            if (email[i].value != null && email[i].value != undefined) {
-                var error = validador.email(email[i].value);
-                if (!error) {
-
-                    errores.push("EL formato del email no es correcto");
-
-                }
+            if (!validador.email(email[n])) {
+                console.log(validador.email(email[n]) + "jaionwerrr");
+                errores.push("El formato del email no es correcto");
             }
 
         }
 
-        if (errores.length > 0) {
+        if (errores.length) {
             e.preventDefault();
-            for (var i = errores.length - 1; i >= 0; i--) {
+
+          /*  for (var i = errores.length - 1; i >= 0; i--) {
                 console.log(errores[i]);
-
-            }
+            };*/
         }
-
     };
 
 
 
-    var validateField() {
+    var validateField = function(event) {
 
-        for (var i = required.length - 1; i >= 0; i--) {
-            var this = required.item(i);
+        //vaciamos el array
+        errores = [];
 
-            if (this.type === "checkbox" && !this.checked) {
 
-                errores.push(this.name + 'No esta marcado');
-
-            } else if (this.type === "password") {
-
-                if (!validador.password(this.value)) {
-                    errores.push(this.name + "el password no cumple con los  requisitos");
-                }
-
+        if (this.type === 'password') {
+            //Comprobamos pass
+            if (!validador.required(this.value)) {
+                //Añadimos el error al array de errores
+                errores.push(this.name + ' es obligatorio');
 
             } else {
-
-                var error = validador.required(this.value);
-                if (!error) {
-                    errores.push(this.name + " Campo Requerido");
+                if (!validador.password(this.value)) {
+                    errores.push(this.name + ' no es una contraseña válida. Debe contener mayúsculas, minúsculas y dígitos');
 
                 }
-
             }
+        } else if (this.type === 'textarea') {
+            if (this.textLength > 50) {
 
-            if(errores= function (){
-                
+                errores.push('Introduzca ' + this.name + ' que no superen los 50 caracteres');
             }
+        } else if (this.type === 'checkbox') {
 
+            if (!this.checked) {
 
+                errores.push(this.name + ' no está marcado');
+            }
+        } else {
+            if (!validador.required(this.value)) {
+
+                errores.push(this.name + ' es obligatorio');
+            }
+        }
+
+        if (errores.length) {
+            showError.call(this, errores.join('\n'));
+        }
+    };
+
+    var showError = function(msgError) {
+
+        var span = document.createElement('span');
+        span.classList.add('help-block');
+        span.innerText = msgError;
+
+        this.parentNode.classList.add('has-error');
+        this.parentNode.appendChild(span);
     }
 
     this.addEventListener('submit', validate, false);
     for (var i = required.length - 1; i >= 0; i--) {
-        required[i].addEventListener('blur',validateField, false);
+        required[i].addEventListener('blur', validateField, false);
     };
 
 
